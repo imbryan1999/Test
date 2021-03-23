b87630d1848f51d6765a4b96d4fc9fe3239c9591
Bryan <hemantshrsth@gmail.com>
2021-03-23 15:05:33 +0530

    Adding logd messages

--- app/src/main/java/com/bryan/testproject/MainActivity.java
+++ app/src/main/java/com/bryan/testproject/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("TAG", "onCreate: Code from master branch.");
+        Log.d("TAG", "onCreate: Code from branch2");
     }
 }
